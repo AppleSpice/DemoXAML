c9485ede7ab1b7045c5c767caf2e31207aa78092
Gaven Rudiak <gavenrudiak@outlook.com>
2020-10-13 10:57:03 -0400

    We Done Lol

--- DemoXAML/MainWindow.xaml.cs
+++ DemoXAML/MainWindow.xaml.cs
@@ -20,9 +20,43 @@ namespace DemoXAML
     /// </summary>
     public partial class MainWindow : Window
     {
+        string yourNameOutput;
+        double mathNumber1;
+        double mathNumber2;
+        double mathFinalAnswer;
+
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            yourNameOutput = YourName.Text;
+            ChangeMe.Text = yourNameOutput;
+
+            mathNumber1 = double.Parse(number1.Text);
+            mathNumber2 = double.Parse(number2.Text);
+            mathFinalAnswer = mathNumber1 + mathNumber2;
+
+            MathAnswer.Text = Convert.ToString(mathFinalAnswer);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeMe.Text = "";
+            YourName.Text = "";
+            number1.Text = "";
+            number2.Text = "";
+            MathAnswer.Text = "";
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            //this.Close();
+            //or 
+            Environment.Exit(0);
         }
     }
 }
